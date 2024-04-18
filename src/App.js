88f7f4687a4c17599a48c98071697e75ce81e837
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-04-18 16:30:16 +0200

    Add routes of the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
 import "./App.css";
+import Register from "./Register";
+import Students from "./Students";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/students" element={<Students />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
