21991627fd565bfc23667cbcb64271f48a9b5755
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-04-22 00:41:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import Register from "./components/Register";
 import StudentList from "./components/StudentList";
 import { Route, Routes } from "react-router-dom";
+import StudentDetail from "./components/StudentDetail";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Register />} />
-      <Route path="/students" element={<StudentList />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/students" element={<StudentList />} />
+        <Route path="/student/:id" element={<StudentDetail />} />
+      </Routes>
+    </>
   );
 }
 
