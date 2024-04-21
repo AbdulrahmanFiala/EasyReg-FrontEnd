23fafa34f404fa21239759f3a41b5e802ac7ba1a
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-04-21 22:08:52 +0200

    Create StudentList.jsx

---
+++ src/components/StudentList.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getStudent, getStudents } from "../api/StudentService";
+import Student from "./Student";
+
+const StudentList = () => {
+  const [data, setData] = useState({});
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    title: "",
+    status: "",
+  });
+
+  const getAllStudents = async () => {
+    try {
+      const { data } = await getStudents();
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  // const updateContact = async (contact) => {
+  //   try {
+  //     const { data } = await saveContact(contact);
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    getAllStudents();
+  }, []);
+
+  return (
+    <main className="student-list">
+      {data?.length === 0 && <div>No Contacts. Please add a new contact</div>}
+      <div class="row">
+        {data?.length > 0 &&
+          data.map((student) => {
+            console.log(student);
+            return <Student student={student} key={student.id} />;
+          })}
+      </div>
+    </main>
+  );
+};
+
+export default StudentList;
