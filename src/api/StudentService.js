212e154d9a78867822db73d8776814b04588657f
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-04-21 22:09:04 +0200

    Create StudentService.js

---
+++ src/api/StudentService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_URL = "https://localhost:7242/api/students/";
+
+export async function saveStudent(student) {
+  return await axios.post(API_URL, student);
+}
+
+export async function getStudents() {
+  return await axios.get(API_URL);
+}
+
+export async function getStudent(id) {
+  return await axios.get(`${API_URL}/${id}`);
+}
+
+export async function updateContact(student) {
+  return await axios.post(API_URL, student);
+}
+
+export async function deleteStudent(id) {
+  return await axios.delete(`${API_URL}/${id}`);
+}
