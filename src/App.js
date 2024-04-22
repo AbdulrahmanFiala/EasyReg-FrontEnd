7cf8e597981d45a24ed6ab5374811b6b68372a89
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-04-22 02:22:34 +0200

    fixed a typo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/students" element={<StudentList />} />
-        <Route path="/student/:id" element={<StudentDetail />} />
+        <Route path="/students/:id" element={<StudentDetail />} />
       </Routes>
     </>
   );
